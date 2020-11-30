cf69bf2ebe23905f832729bb8958ac9fe04a2324
anzor_quantori <anzor.gurchumelia@quantori.com>
2020-11-30 15:34:34 +0400

    Anzori-Ghurtchumelia_hw2_implementation

--- Java2020/src/lesson201130/ArraysClassUsage.java
+++ Java2020/src/lesson201130/ArraysClassUsage.java
@@ -29,8 +29,32 @@ public class ArraysClassUsage {
 	}
 
 	private static int[] insert(int[] a, int key, int index) {
-		// TODO this is your home work
-		return null;
+		// increment the size of new array by one
+		int[] newArray = new int[a.length + 1]; 
+		// if the index is negative or equal to zero, I put the key in the beginning of the array and shift other values to the right 
+		if(index <= 0) { 
+			newArray[0] = key; 
+			for(int i = 1; i < a.length; i++) {
+				newArray[i] = a[i-1]; 
+				} 	
+			} 
+		// if the index is greater than the length of the array, I put the key in the end of the array and leave other values untouched
+		else if(index > a.length){ 
+				newArray[newArray.length - 1] = key; 
+				for(int i = 0; i < a.length; i++) { 
+					newArray[i] = a[i]; 
+					} 
+				} 
+		// if the index is in between 1 and the length of the array, I put the key at the 'a[index]' and shift other values to the right
+		else { 
+			newArray[index] = key; 
+			for(int i = 0; i < newArray.length; i++) { 
+				if(newArray[i] == 0 && i < index) newArray[i] = a[i]; 
+				else if(i > index) newArray[i] = a[i-1]; 
+				else continue; 
+			}	
+		}
+		return newArray;	
 	}
 
 }
