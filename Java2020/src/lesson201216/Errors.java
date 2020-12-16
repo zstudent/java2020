ebf0ddd23497f03fdc1fb29e14fab7f695878644
Zaal <zaal@quantori.com>
2020-12-16 13:55:41 +0400

    Lesson 2020-12-16 Exceptions 1

---
+++ Java2020/src/lesson201216/Errors.java
@@ -0,0 +1,15 @@
+package lesson201216;
+
+import java.lang.AssertionError;
+import java.lang.String;
+
+public class Errors {
+	
+	public static void main(String[] args) {
+		
+		
+		throw new AssertionError();
+		
+	}
+
+}
