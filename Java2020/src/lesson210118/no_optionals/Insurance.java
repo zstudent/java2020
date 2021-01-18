572bb06e9298a1ce71d04f34581c1aab6007662d
Zaal <zaal@quantori.com>
2021-01-18 16:18:09 +0400

    Lesson 2021-01-18 Optionals Part 2

---
+++ Java2020/src/lesson210118/no_optionals/Insurance.java
@@ -0,0 +1,11 @@
+package lesson210118.no_optionals;
+
+public class Insurance {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ Java2020/src/lesson210118/no_optionals/Person.java
@@ -0,0 +1,10 @@
+package lesson210118.no_optionals;
+
+public class Person {
+	private Car car;
+
+	public Car getCar() {
+		return car;
+	}
+
+}
